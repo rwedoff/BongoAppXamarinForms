24a950b4f279947cb425604a0b39977f1d4382b7
rwedoff <rwedoff@DESKTOP-4TKHSSO>
2016-09-19 14:38:56 -0500

    Add project files.

---
+++ BongoApp/BongoApp.UWP/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BongoApp.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new BongoApp.App());
+
+        }
+    }
+}
---
+++ BongoApp/BongoApp/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace BongoApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            GetBongoData();
+        }
+
+        public async void GetBongoData()
+        {
+            HttpClient client = new HttpClient();
+            var uri = new Uri(@"http://api.ebongo.org/routelist?format=json&api_key=XXXX");
+            var response = await client.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                asdfasdfasdf.Text = content;
+            }
+        }
+    }
+}
